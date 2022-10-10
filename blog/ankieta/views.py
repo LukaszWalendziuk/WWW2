bfe5546a0c90a23ea20143f2190e074d1fb7e75c
LukaszWalendziuk <113417353+LukaszWalendziuk@users.noreply.github.com>
2022-10-10 12:15:17 +0200

    Add files via upload

--- blog/ankieta/views.py
+++ blog/ankieta/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Question
 
-# Create your views here.
+
+def question_list(request):
+   questions = Question.objects.all()
+   return render(request, 'ankieta/question_list.html', {'questions': questions})
+
+def question_detail(request, question_id):
+   question = get_object_or_404(Question, pk=question_id)
+   return render(request, 'ankieta/question_detail.html', {'question': question})
